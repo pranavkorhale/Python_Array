f7ff43b5dfbf2571e29397f36bd8452f694e0fd6
umangag07 <52677699+umangag07@users.noreply.github.com>
2020-06-02 16:06:42 +0530

    creation of array
    Array can be created from the list and the tuple.

---
+++ creating array.py
@@ -0,0 +1,22 @@
+import numpy 
+"""
+creating the array from the list
+
+"""
+Array1=numpy.array([1,2,3,4,5,6])
+
+"""
+creating the array from the tuple
+
+"""
+Array2=numpy.array((10,11,12,13,14,15))
+
+print(Array1)
+print(Array2)
+
+"""
+To check whether array is created or list
+
+"""
+print("Type of data of array1",type(Array1))
+print("Type of data array2",type(Array2))
